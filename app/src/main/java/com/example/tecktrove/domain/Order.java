007c4928d97595d3935b4499c17d0d36981e64a8
MariaSchoinaki <shinaki54@gmail.com>
2023-12-03 13:30:24 +0200

    created Order,Orderline classes

---
+++ app/src/main/java/com/example/tecktrove/domain/Order.java
@@ -0,0 +1,49 @@
+package com.example.tecktrove.domain;
+
+import android.telecom.TelecomManager;
+
+public class Order {
+    private Date date;
+    private Money total;
+    private int cardNumber;
+    private Telephone telephone;
+    private Mail email;
+
+    public Date getDate(){
+        return this.date;
+    }
+
+   public Money getTotal(){
+        return this.total;
+    }
+
+   public int getCardNumber(){
+        return this.cardNumber;
+    }
+
+   public Telephone getTelephone(){
+        return this.telephone;
+    }
+
+    public Mail getEmail() {
+        return email;
+    }
+
+    void setDate(Date date) {
+        this.date = date;
+    }
+
+    void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+     void setTelephone(Telephone telephone) {
+        this.telephone = telephone;
+    }
+
+    void setEmail(Mail email) {
+        this.email = email;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tecktrove/domain/OrderLine.java
@@ -0,0 +1,26 @@
+package com.example.tecktrove.domain;
+
+public class OrderLine {
+    private int quantity;
+    private Money subTotal;
+    private ProductType ProductType;
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void getProductType(){
+        this.ProductType;
+    }
+
+    public Money getSubTotal() {
+        return subTotal;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+}
