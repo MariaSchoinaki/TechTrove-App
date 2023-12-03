06db622de6d56de1d8fc38e3b553ff365a97dea5
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-03 18:32:49 +0200

    created UserTest

---
+++ app/src/test/java/com/example/tecktrove/domain/UserTest.java
@@ -0,0 +1,39 @@
+package com.example.tecktrove.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import com.example.tecktrove.contacts.Email;
+import com.example.tecktrove.contacts.Telephone;
+
+import org.junit.*;
+
+public class UserTest {
+
+    private User user1;
+    private User user2;
+
+    @Before
+    public void setUp(){
+        this.user1 = new User(1, "george15", "12345", "George", "Papageorge", new Email("george@gmail.com"), new Telephone("6987402775"));
+        this.user2 = new User(45, "leana", "les34", "Leana", "Triantafillidi", new Email("lean@gmail.com"), new Telephone("6987563456"));
+    }
+
+    @Test
+    public void checkGetSet(){
+        assertNotEquals(this.user1.getEmail(), this.user2.getEmail());
+        assertNotEquals(this.user1.getId(),this.user2.getId());
+        assertNotEquals(this.user1.getName(),this.user2.getName());
+        assertNotEquals(this.user1.getTelephone(),this.user2.getTelephone());
+        assertNotEquals(this.user1.getPassword(),this.user2.getPassword());
+        assertNotEquals(this.user1.getUsername(),this.user2.getUsername());
+        assertNotEquals(this.user1.getLastName(),this.user2.getLastName());
+
+        this.user1.setName("Leana");
+        assertEquals(this.user1.getName(), this.user2.getName());
+
+        assertEquals(user1,user1);
+        assertNotEquals(user1, new User());
+
+    }
+}
