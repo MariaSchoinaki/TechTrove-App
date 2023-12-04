ec6d4465b0e1bb54e0e5c285e5c0ca110de01515
MariaSchoinaki <shinaki54@gmail.com>
2023-12-04 11:58:23 +0200

    created Port.java

---
+++ app/src/main/java/com/example/tecktrove/util/Port.java
@@ -0,0 +1,14 @@
+package com.example.tecktrove.util;
+
+import android.util.Pair;
+import java.util.ArrayList;
+
+/**
+ * The main purpose of this class is to provide
+ * an easy and safe way to implement the ports of a component.
+ */
+public class Port {
+
+    private ArrayList<Pair<String, Integer>> ports;
+    Port(){}
+}
