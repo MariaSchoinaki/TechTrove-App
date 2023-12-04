07b8a7d7ba14de6bb2c15078cdc62b9ec004c297
ChristosStamoulos <stamcrist2@outlook.com>
2023-12-04 23:54:39 +0200

    Edited ProductTypeTest added SimpleCalendarTest

--- app/src/test/java/com/example/tecktrove/domain/ProductTypeTest.java
+++ app/src/test/java/com/example/tecktrove/domain/ProductTypeTest.java
@@ -1,4 +1,44 @@
 package com.example.tecktrove.domain;
 
+import org.junit.Before;
+
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.tecktrove.util.Money;
+
+import java.util.Currency;
+import java.math.BigDecimal;
+import org.junit.*;
+
+
+
 public class ProductTypeTest {
+    private Currency euroCurrency = Currency.getInstance("EUR");
+    ProductType t1,t2;
+    @Before
+    public void Setup(){
+        t1 = new ProductType(25,new Money(new BigDecimal(2100.0),euroCurrency),"apple");
+        t2 = new ProductType(26,new Money(new BigDecimal(2100.1),euroCurrency),"arp");
+    }
+    @Test
+    public void checkDefaultConstructor(){
+        ProductType t3 = new ProductType();
+        t3.setModelNo(23);
+        t3.setPrice(new Money(new BigDecimal(2100.0),euroCurrency));
+        t3.setName("a");
+        assertEquals(23,t3.getModelNo());
+        assertEquals(new Money(new BigDecimal(2100.0),euroCurrency),t3.getPrice());
+        assertEquals("a",t3.getName());
+    }
+
+    @Test
+    public void checkGetSet(){
+        t2.setModelNo(25);
+        t2.setPrice(new Money(new BigDecimal(2100.0),euroCurrency));
+        t2.setName("apple");
+        assertEquals(this.t1.getModelNo(),this.t2.getModelNo());
+        assertEquals(this.t1.getPrice(),this.t2.getPrice());
+        assertEquals(this.t1.getName(),this.t2.getName());
+    }
 }
