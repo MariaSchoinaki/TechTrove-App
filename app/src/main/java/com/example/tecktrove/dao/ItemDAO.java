258fef2bb3855e529087eecb67242b8b4b26e503
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-16 15:16:56 +0200

    added comments in ItemDAO and ItemMemoryDAO

--- app/src/main/java/com/example/tecktrove/dao/ItemDAO.java
+++ app/src/main/java/com/example/tecktrove/dao/ItemDAO.java
@@ -7,18 +7,52 @@ import java.util.List;
 
 public interface ItemDAO {
 
+    /**
+     * Gets the item for some input number.
+     *
+     * @param serialNo  the input number
+     * @return          the item
+     */
     Item find(int serialNo);
 
+    /**
+     * Gets a list of all the items in the dao
+     *
+     * @return
+     */
     ArrayList<Item> findAll();
 
+    /**
+     * Saves an object item in the dao
+     *
+     * @param entity    the item object
+     */
     void save(Item entity);
 
+    /**
+     * Deletes the item object from the dao if it exists
+     *
+     * @param entity    an item object
+     */
     void delete(Item entity);
 
+    /**
+     * Deletes the item object from the dao if it exists based on the serial number
+     *
+     * @param serialNo      the serial number of the item
+     */
     void delete(int serialNo);
 
+    /**
+     * Deletes all the items from the dao
+     */
     void deleteAll();
 
+    /**
+     * Returns the next available id that can be used for an item object
+     *
+     * @return  the next available number
+     */
     int nextId();
 
 }
--- app/src/main/java/com/example/tecktrove/memorydao/ItemDAOMemory.java
+++ app/src/main/java/com/example/tecktrove/memorydao/ItemDAOMemory.java
@@ -9,6 +9,12 @@ public class ItemDAOMemory implements ItemDAO {
 
     protected ArrayList<Item> items = new ArrayList<Item>();
 
+    /**
+     * Gets the item for some input number.
+     *
+     * @param serialNo  the input number
+     * @return          the item
+     */
     @Override
     public Item find(int serialNo){
         for (Item item: items){
@@ -19,31 +25,59 @@ public class ItemDAOMemory implements ItemDAO {
         return null;
     }
 
+    /**
+     * Gets a list of all the items in the dao
+     *
+     * @return
+     */
     @Override
     public ArrayList<Item> findAll(){
         return new ArrayList<Item>(items);
     }
 
+    /**
+     * Saves an object item in the dao
+     *
+     * @param entity    the item object
+     */
     @Override
     public void save(Item entity){
         items.add(entity);
     }
 
+    /**
+     * Deletes the item object from the dao if it exists
+     *
+     * @param entity    an item object
+     */
     @Override
     public void delete(Item entity){
         items.remove(entity);
     }
 
+    /**
+     * Deletes the item object from the dao if it exists based on the serial number
+     *
+     * @param serialNo      the serial number of the item
+     */
     @Override
     public void delete(int serialNo){
         items.remove(find(serialNo));
     }
 
+    /**
+     * Deletes all the items from the dao
+     */
     @Override
     public void deleteAll(){
         items.clear();
     }
 
+    /**
+     * Returns the next available id that can be used for an item object
+     *
+     * @return  the next available number
+     */
     @Override
     public int nextId(){
         return (items.size() > 0 ? items.get(items.size() - 1).getSerialNo() + 1 : 1);
