f767bf2782d63011a8312075e10f4f24cd948ca5
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-19 22:57:11 +0200

    added methods
    added methods
    -getPassword: gets input password as string
    -getUsername: gets input username as string
    -onStart: when log in button is pressed the process of log in is starting
    -login: if log in is successful move to a differrent activity (SignUpActivity is a placeholder)

--- app/src/main/java/com/example/tecktrove/view/LogIn/LogInActivity.java
+++ app/src/main/java/com/example/tecktrove/view/LogIn/LogInActivity.java
@@ -2,14 +2,19 @@ package com.example.tecktrove.view.LogIn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.tecktrove.R;
 import com.example.tecktrove.view.SignUp.SignUpActivity;
+import com.example.tecktrove.view.StartScreen.StartScreenActivity;
 
 public class LogInActivity extends AppCompatActivity implements LogInView {
+
+    LogInPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,7 @@ public class LogInActivity extends AppCompatActivity implements LogInView {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_user_login);
 
-        LogInPresenter presenter = new LogInPresenter(this);
+        presenter = new LogInPresenter(this);
         findViewById(R.id.txtSignUp).setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -26,7 +31,43 @@ public class LogInActivity extends AppCompatActivity implements LogInView {
             }
         });
     }
+
     public void signUp(){
+        Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        findViewById(R.id.btnLogIn).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                presenter.startProcess();
+            }
+        });
+    }
+
+    @Override
+    public String getUsername() {
+        return ((EditText)findViewById(R.id.logIn_username)).getText().toString().trim();
+    }
+
+    @Override
+    public String getPassword() {
+        return ((EditText)findViewById(R.id.logIn_password)).getText().toString().trim();
+    }
+
+    @Override
+    public void showErrorMessage(String title, String msg) {
+        new AlertDialog.Builder(LogInActivity.this)
+                .setCancelable(true)
+                .setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton(R.string.ok, null).create().show();
+    }
+
+    @Override
+    public void login(){
         Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
         startActivity(intent);
     }
