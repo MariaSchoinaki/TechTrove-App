1e9fb1aaa0498b804ab12c04bce0cae5c041afe7
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-19 22:57:52 +0200

    Update LogInView.java

--- app/src/main/java/com/example/tecktrove/view/LogIn/LogInView.java
+++ app/src/main/java/com/example/tecktrove/view/LogIn/LogInView.java
@@ -3,4 +3,12 @@ package com.example.tecktrove.view.LogIn;
 public interface LogInView {
 
     void signUp();
+
+    String getUsername();
+
+    String getPassword();
+
+    void showErrorMessage(String title, String msg);
+
+    void login();
 }
