4828af963590529387a20c85301d7bd0da72fb5c
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-20 18:37:08 +0200

    created basic implementation of Factory

---
+++ app/src/main/java/com/example/tecktrove/dao/DAOFactory.java
@@ -0,0 +1,37 @@
+package com.example.tecktrove.dao;
+
+public abstract class DAOFactory {
+
+    private static DAOFactory factory = null;
+
+    public static DAOFactory getFactory(){
+        if (factory == null) {
+            String className = null;
+
+            if (System.getProperty("daofactory") != null) {
+                className = System.getProperty("daofactory");
+            }
+
+            try {
+                factory = (DAOFactory) Class.forName(className).newInstance();
+            } catch (Exception e) {
+                //throw new TecktroveException(e);
+            }
+        }
+        return factory;
+    }
+
+    public abstract CustomerDAO getCustomerDAO();
+
+    public abstract EmployerDAO getEmployerDAO();
+
+    public abstract ComponentDAO getComponentDAO();
+
+    public abstract SynthesisDAO getSynthesisDAO();
+
+    public abstract OrderDAO getOrderDAO();
+
+    public abstract OrderLineDAO getOrderLineDAO();
+
+    public abstract ItemDAO getItemDAO();
+}
---
+++ app/src/main/java/com/example/tecktrove/memorydao/MemoryDAOFactory.java
@@ -0,0 +1,4 @@
+package com.example.tecktrove.memorydao;
+
+public class MemoryDAOFactory {
+}
