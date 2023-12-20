25427769b98d48fe12fa611d9917e40087e87271
Chris  Stam <chris@192.168.1.20>
2023-12-21 00:01:57 +0200

    update SignUpActivity , SignUpView

--- app/src/main/java/com/example/tecktrove/view/SignUp/SignUpActivity.java
+++ app/src/main/java/com/example/tecktrove/view/SignUp/SignUpActivity.java
@@ -2,14 +2,26 @@ package com.example.tecktrove.view.SignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import com.example.tecktrove.R;
+import com.example.tecktrove.memorydao.MemoryInitializer;
 import com.example.tecktrove.view.LogIn.LogInActivity;
+import com.example.tecktrove.view.LogIn.LogInPresenter;
+import com.example.tecktrove.view.StartScreen.StartScreenActivity;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity implements SignUpView {
+    SignUpPresenter presenter;
+    ArrayList<String> info = new ArrayList<String>();
+    MemoryInitializer init;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,9 @@ public class SignUpActivity extends AppCompatActivity implements SignUpView {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_user_signup);
 
-        SignUpPresenter presenter = new SignUpPresenter(this);
+        init = new MemoryInitializer();
+        presenter = new SignUpPresenter(this,init.getCustomerDAO(), init.getEmployerDAO());
+
         findViewById(R.id.txtSignIn).setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -27,8 +41,63 @@ public class SignUpActivity extends AppCompatActivity implements SignUpView {
         });
     }
 
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        findViewById(R.id.signup_button).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                    presenter.startProcess(info);
+
+            }
+        });
+
+
+    }
+
     public void logIn(){
         Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public String getUsername() {
+        return ((EditText)findViewById(R.id.logIn_username)).getText().toString().trim();
+    }
+
+    @Override
+    public String getPassword() {
+        return ((EditText)findViewById(R.id.logIn_password)).getText().toString().trim();
+    }
+
+    @Override
+    public String fullName() {
+        return ((EditText)findViewById(R.id.SignUpFullName)).getText().toString().trim();
+    }
+
+    @Override
+    public String getEmail() {
+        return ((EditText)findViewById(R.id.SignUpEmail)).getText().toString().trim();
+    }
+
+    @Override
+    public String getTelephone() {
+        return ((EditText)findViewById(R.id.edtSignUpMobile)).getText().toString().trim();
+    }
+
+
+    public void showErrorMessage(String title, String msg) {
+        new AlertDialog.Builder(SignUpActivity.this)
+                .setCancelable(true)
+                .setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton(R.string.ok, null).create().show();
+    }
+
+    @Override
+    public void signUp(){
+        Intent intent = new Intent(SignUpActivity.this, StartScreenActivity.class);
         startActivity(intent);
     }
 }
