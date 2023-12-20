cc06a6f780b2408d4ccdf18c1940aa5aa5a1fff3
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-20 23:14:54 +0200

    added startProcess

--- app/src/main/java/com/example/tecktrove/view/SignUp/SignUpPresenter.java
+++ app/src/main/java/com/example/tecktrove/view/SignUp/SignUpPresenter.java
@@ -1,11 +1,35 @@
 package com.example.tecktrove.view.SignUp;
 
+import com.example.tecktrove.dao.CustomerDAO;
+import com.example.tecktrove.dao.EmployerDAO;
+import com.example.tecktrove.util.Money;
+
+import java.util.ArrayList;
+
 public class SignUpPresenter {
     SignUpView view;
+    CustomerDAO customers;
+    EmployerDAO employers;
 
-    public SignUpPresenter(SignUpView view){
+    public SignUpPresenter(SignUpView view, CustomerDAO customers, EmployerDAO employers){
         this.view = view;
+        this.customers = customers;
+        this.employers = employers;
     }
 
     void onLogIn(){ view.logIn(); }
+
+    public void startProcess(ArrayList<String> info){
+        boolean allgood = true;
+
+        for(String field: info){
+            if(field.equals("")) {
+                allgood = false;
+            }
+        }
+
+        if(!allgood) {
+            view.showErrorMessage("Error", "Please fill all the fields.");
+        }
+    }
 }
