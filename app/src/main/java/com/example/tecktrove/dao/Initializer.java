b797487ce82ec8cfc5bcce0371ef1c7f079abe1b
ChristosStamoulos <stamcrist2@outlook.com>
2023-12-29 17:01:40 +0200

    added ports

--- app/src/main/java/com/example/tecktrove/dao/Initializer.java
+++ app/src/main/java/com/example/tecktrove/dao/Initializer.java
@@ -10,6 +10,7 @@ import com.example.tecktrove.domain.ProductType;
 import com.example.tecktrove.domain.Synthesis;
 import com.example.tecktrove.domain.Component;
 import com.example.tecktrove.domain.Item;
+import com.example.tecktrove.util.Pair;
 import com.example.tecktrove.util.Port;
 import com.example.tecktrove.util.Money;
 import com.example.tecktrove.util.SimpleCalendar;
@@ -60,67 +61,165 @@ public abstract class Initializer {
         Employer em3 = new Employer(8596, "kostas34", "13k023m32", "Konstantinos", "Glitsas", new Email("glitsas13@gmail.com"), new Telephone("6945259875"));
         getEmployerDAO().save(em3);
 
+
         //Components
+
         Component com1 = new Component(4191, Money.euros(BigDecimal.valueOf(59.99)), "Case Kolink VOID RGB Midi Tower", "Το VOID Midi-Tower Case αντιπροσωπεύει ένα συναρπαστικό νέο κεφάλαιο στην ιστορία της Kolink, με εντυπωσιακό εφέ «απείρου» καθρεπτισμού σε σχήμα V, ανεμιστήρα 120mm ARGB και πληθώρα χαρακτηριστικών.","Kolink" , new Port(), new Port(), 80);
         getComponentDAO().save(com1);
 
-        Component com2 = new Component(2936, Money.euros(BigDecimal.valueOf(36.90)), "Turbo-X PSU Value III Series 550 W", "Προσιτό αλλά αξιόπιστο τροφοδοτικό, με ισχύ 550W, προηγμένες δικλείδες ασφαλείας και αθόρυβο ανεμιστήρα 120mm.", "Turbo-X", new Port(), new Port(), 75);
+        Pair pair2_1 = new Pair("ATX Power Port",1);
+        Pair pair2_2 = new Pair("ATX 12V Power Port",1);
+        Pair pair2_3 = new Pair("SATA Power Port",2);
+        Pair pair2_4 = new Pair("Molex Connector",1);
+        Pair pair2_5 = new Pair("PCI Express Connector",1);
+        Pair pair2_6 = new Pair("PCI Floppy Drive Connector",1);
+        Pair pair2_7 = new Pair("AC Adapter",1);
+
+        Port port2 = new Port();
+        Port port2_1 = new Port();
+
+        port2.add(pair2_1);
+        port2.add(pair2_2);
+        port2.add(pair2_3);
+        port2.add(pair2_4);
+        port2.add(pair2_5);
+        port2.add(pair2_6);
+        port2_1.add(pair2_7);
+        Component com2 = new Component(2936, Money.euros(BigDecimal.valueOf(36.90)), "Turbo-X PSU Value III Series 550 W", "Προσιτό αλλά αξιόπιστο τροφοδοτικό, με ισχύ 550W, προηγμένες δικλείδες ασφαλείας και αθόρυβο ανεμιστήρα 120mm.", "Turbo-X",  port2, port2_1, 75);
         getComponentDAO().save(com2);
         Money total_cost1 = com1.getPrice().plus(com2.getPrice());
 
-        Component com3 = new Component(3260, Money.euros(BigDecimal.valueOf(119.90)), "AMD CPU Ryzen 3 3200G", "Με τέσσερις πυρήνες Zen σε Socket AM4, μέγιστη συχνότητα λειτουργίας 4GHz, μνήμη cache 6MB και Radeon Vega 8 iGPU για αξεπέραστες επιδόσεις γραφικών.", "Turbo-X", new Port(), new Port(), 60);
+        Pair pair3_1 = new Pair("socket AM4",1);
+        Port port3 = new Port();
+        port3.add(pair3_1);
+        Component com3 = new Component(3260, Money.euros(BigDecimal.valueOf(119.90)), "AMD CPU Ryzen 3 3200G", "Με τέσσερις πυρήνες Zen σε Socket AM4, μέγιστη συχνότητα λειτουργίας 4GHz, μνήμη cache 6MB και Radeon Vega 8 iGPU για αξεπέραστες επιδόσεις γραφικών.", "AMD",port3,port3, 60);
         getComponentDAO().save(com3);
         total_cost1 = total_cost1.plus(com3.getPrice());
 
-        Component com4 = new Component(3888, Money.euros(BigDecimal.valueOf(32.90)), "Alpenföhn CPU Cooler Ben Nevis", "Ψύκτρα με ανεμιστήρα 130mm συμβατή με τα sockets 2066, 2011, 2011-v3, 1366, 115X, 1200, AM4, 775, AM4, AM3(+), AM3, AM2(+), AM2 και FM1.", "Alpenföhn", new Port(), new Port(), 20);
+        Pair pair4_1 = new Pair("AM4,",1);
+        Port port4 = new Port();
+        port4.add(pair4_1);
+        Component com4 = new Component(3888, Money.euros(BigDecimal.valueOf(32.90)), "Alpenföhn CPU Cooler Ben Nevis", "Ψύκτρα με ανεμιστήρα 130mm συμβατή με τα sockets 2066, 2011, 2011-v3, 1366, 115X, 1200, AM4, 775, AM4, AM3(+), AM3, AM2(+), AM2 και FM1.", "Alpenföhn", new Port(), port4, 20);
         getComponentDAO().save(com4);
         total_cost1 = total_cost1.plus(com4.getPrice());
 
-        Component com5 = new Component(4188, Money.euros(BigDecimal.valueOf(129.90)), "Gigabyte Motherboard A520I AC", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει DDR4 RAM ως 64GB, NVMe PCIe 3.0 M.2 και Intel® Dual Band 802.11ac Wi-Fi.", "AMD", new Port(), new Port(), 57);
+        Pair pair5_1 = new Pair("socket AM4",1);
+        Pair pair5_2 = new Pair("DDR4",2);
+        Pair pair5_3 = new Pair("PCI Express x16 3.0",1);
+        Pair pair5_4 = new Pair("SATA III",4);
+
+        Port port5 = new Port();
+
+        port5.add(pair5_1);
+        port5.add(pair5_2);
+        port5.add(pair5_3);
+        port5.add(pair5_4);
+        Component com5 = new Component(4188, Money.euros(BigDecimal.valueOf(129.90)), "Gigabyte Motherboard A520I AC", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει DDR4 RAM ως 64GB, NVMe PCIe 3.0 M.2 και Intel® Dual Band 802.11ac Wi-Fi.", "AMD",port5, new Port(), 57);
         getComponentDAO().save(com5);
         total_cost1 = total_cost1.plus(com5.getPrice());
 
-        Component com6 = new Component(3935, Money.euros(BigDecimal.valueOf(44.90)), "Crucial Desktop RAM Value 16GB 3200MHz DDR4", "Μνήμη DDR4-3200 UDIMM χωρητικότητας 16GB από την Crucial® με συχνότητα λειτουργίας 3.200 MHz και CL22.", "Micron", new Port(), new Port(), 44);
+        Pair pair6 = new Pair("DDR4",1);
+        Port port6 = new Port();
+
+        port6.add(pair6);
+        Component com6 = new Component(3935, Money.euros(BigDecimal.valueOf(44.90)), "Crucial Desktop RAM Value 16GB 3200MHz DDR4", "Μνήμη DDR4-3200 UDIMM χωρητικότητας 16GB από την Crucial® με συχνότητα λειτουργίας 3.200 MHz και CL22.", "Micron",new Port(),port6, 44);
         getComponentDAO().save(com6);
         total_cost1 = total_cost1.plus(com6.getPrice());
 
-        Component com7 = new Component(4311, Money.euros(BigDecimal.valueOf(69.90)), "Asus VGA GeForce GT 730 Evo Low Profile BRK 2 GB", "Είναι ιδανική για υπολογιστές γραφείου, Small Form Factor (SFF) ή Home Theater PCs (HTPC). Είναι χαμηλής κατανάλωσης, με παθητική ψύξη και διαθέτει 3x εξόδους εικόνας.", "ASUS", new Port(), new Port(), 28);
+        Pair pair7_1 = new Pair("PCI Express x16 2.0",1);
+        Pair pair7_2 = new Pair("HDMI",1);
+        Pair pair7_3 = new Pair("DVI-D",1);
+
+        Port port7 = new Port();
+        Port port7_1 = new Port();
+
+        port7.add(pair7_3);
+        port7.add(pair7_2);
+        port7_1.add(pair7_1);
+        Component com7 = new Component(4311, Money.euros(BigDecimal.valueOf(69.90)), "Asus VGA GeForce GT 730 Evo Low Profile BRK 2 GB", "Είναι ιδανική για υπολογιστές γραφείου, Small Form Factor (SFF) ή Home Theater PCs (HTPC). Είναι χαμηλής κατανάλωσης, με παθητική ψύξη και διαθέτει 3x εξόδους εικόνας.", "ASUS",  port7,port7_1, 28);
         getComponentDAO().save(com7);
         total_cost1 = total_cost1.plus(com7.getPrice());
 
-        Component com8 = new Component(2489, Money.euros(BigDecimal.valueOf(26.90)), "SanDisk SSD Plus 240GB", "Η τεχνολογία SSD 2,5” στο φορητό ή το σταθερό σου υπολογιστή, με χωρητικότητα 240GB, γρήγορες ταχύτητες λειτουργίας και σύνδεση SATA III.", "SanDisk", new Port(), new Port(), 35);
+        Pair pair8 = new Pair("SATA III",1);
+        Port port8 = new Port();
+
+        port8.add(pair8);
+        Component com8 = new Component(2489, Money.euros(BigDecimal.valueOf(26.90)), "SanDisk SSD Plus 240GB", "Η τεχνολογία SSD 2,5” στο φορητό ή το σταθερό σου υπολογιστή, με χωρητικότητα 240GB, γρήγορες ταχύτητες λειτουργίας και σύνδεση SATA III.", "SanDisk",new Port(),port8, 35);
         getComponentDAO().save(com8);
         total_cost1 = total_cost1.plus(com8.getPrice());
 
-
         Component com11 = new Component(3604, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon Midi ATX Tower VS4-V Midi Tower", "Μινιμαλιστική σχεδίαση, ευρύχωρο εσωτερικό και άφθονες θέσεις για SSDs/HDDs, προεγκατεστημένος ανεμιστήρας και φίλτρο σκόνης, πρακτικό πάνελ και συμβατότητα με συστήματα υδρόψυξης.", "Sharkoon", new Port(), new Port(), 10);
         getComponentDAO().save(com11);
+        Pair pair12_1 = new Pair("ATX Power Port",1);
+        Pair pair12_2 = new Pair("ATX 12V Power Port",1);
+        Pair pair12_3 = new Pair("SATA Power Port",2);
+        Pair pair12_4 = new Pair("PCI Express Connector",1);
 
-        Component com12 = new Component(3740, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon PSU Series 500 W 80+ Bronze", "Τροφοδοτικό υπολογιστή Sharkoon 500W με πιστοποίηση 80+ Bronze, active PFC, αθόρυβη λειτουργεία με ανεμιστήρα 120mm και διακριτική μαύρη εμφάνιση.", "Sharkoon", new Port(), new Port(), 17);
+        Port port12 = new Port();
+
+        port12.add(pair12_1);
+        port12.add(pair12_2);
+        port12.add(pair12_3);
+        port12.add(pair12_4);
+        Component com12 = new Component(3740, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon PSU Series 500 W 80+ Bronze", "Τροφοδοτικό υπολογιστή Sharkoon 500W με πιστοποίηση 80+ Bronze, active PFC, αθόρυβη λειτουργεία με ανεμιστήρα 120mm και διακριτική μαύρη εμφάνιση.", "Sharkoon",  port12,port2_1, 17);
         getComponentDAO().save(com12);
         Money total_cost2 = com11.getPrice().plus(com12.getPrice());
 
-        Component com13 = new Component(3740, Money.euros(BigDecimal.valueOf(84.90)), "AMD CPU Ryzen 5 4500", "Με πυρήνες αρχιτεκτονικής Zen 2 και υψηλότερους χρονισμούς, περισσότερο bandwidth, υποστήριξη PCIe 3.0 και αποκλειστικές τεχνολογίες AMD, επαναπροσδιορίζουν την απόδοση των Gaming PCs.", "AMD", new Port(), new Port(), 33);
+        Pair pair13_1 = new Pair("socket AM4+",1);
+        Port port13 = new Port();
+        port13.add(pair13_1);
+        Component com13 = new Component(3740, Money.euros(BigDecimal.valueOf(84.90)), "AMD CPU Ryzen 5 4500", "Με πυρήνες αρχιτεκτονικής Zen 2 και υψηλότερους χρονισμούς, περισσότερο bandwidth, υποστήριξη PCIe 3.0 και αποκλειστικές τεχνολογίες AMD, επαναπροσδιορίζουν την απόδοση των Gaming PCs.", "AMD",port13,port13,33);
         getComponentDAO().save(com13);
         total_cost2 = total_cost2.plus(com13.getPrice());
 
-        Component com14 = new Component(4188, Money.euros(BigDecimal.valueOf(34.90)), "Be Quiet! Be Quiet Light Wings 140mm PWM", "Ανεμιστήρας 140mm με PWM, 7 ειδικά βελτιστοποιημένα για χαμηλό θόρυβο και υψηλή απόδοση πτερύγια, rifle-bearing και δύο φωτιζόμενους δακτυλίους ARGB.", "Be Quiet!", new Port(), new Port(), 54);
+        Pair pair14_1 = new Pair("4-Pin PWM",1);
+        Port port14 = new Port();
+        port14.add(pair14_1);
+        Component com14 = new Component(4188, Money.euros(BigDecimal.valueOf(34.90)), "Be Quiet! Be Quiet Light Wings 140mm PWM", "Ανεμιστήρας 140mm με PWM, 7 ειδικά βελτιστοποιημένα για χαμηλό θόρυβο και υψηλή απόδοση πτερύγια, rifle-bearing και δύο φωτιζόμενους δακτυλίους ARGB.", "Be Quiet!", new Port(),  port14, 54);
         getComponentDAO().save(com14);
         total_cost2 = total_cost2.plus(com14.getPrice());
 
-        Component com15 = new Component(3639, Money.euros(BigDecimal.valueOf(69.90)), "MSI Motherboard A520M-A PRO", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει μνήμη DDR4 ως 64GB και έχει υποδοχές Μ.2 και PCI Express Gen3 x16.", "AMD", new Port(), new Port(), 85);
+        Pair pair15_1 = new Pair("socket AM4+",1);
+        Pair pair15_2 = new Pair("DDR5",2);
+        Pair pair15_3 = new Pair("PCI Express x16 3.0",1);
+        Pair pair15_4 = new Pair("SATA III",4);
+
+        Port port15 = new Port();
+
+        port15.add(pair15_1);
+        port15.add(pair15_2);
+        port15.add(pair15_3);
+        port15.add(pair15_4);
+        Component com15 = new Component(3639, Money.euros(BigDecimal.valueOf(69.90)), "MSI Motherboard A520M-A PRO", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει μνήμη DDR3 ως 64GB και έχει υποδοχές Μ.2 και PCI Express Gen3 x16.", "AMD", port15, new Port(), 85);
         getComponentDAO().save(com15);
         total_cost2 = total_cost2.plus(com15.getPrice());
 
-        Component com16 = new Component(3290, Money.euros(BigDecimal.valueOf(69.90)), "Corsair Desktop RAM Vengeance PRO RGB 16GB Kit 3000MHz DDR4", "16GB μνήμης RAM DDR4 για υψηλές επιδόσεις και χρονισμούς, με built-in heat spreaders και multi-zone RGB φωτισμό που συμπληρώνει το στυλ κάθε gamer.", "Corsair", new Port(), new Port(), 29);
+        Pair pair16 = new Pair("DDR5",1);
+        Port port16 = new Port();
+        port16.add(pair16);
+        Component com16 = new Component(3290, Money.euros(BigDecimal.valueOf(69.90)), "Corsair Desktop RAM Vengeance PRO RGB 16GB Kit 3000MHz DDR5", "16GB μνήμης RAM DDR5 για υψηλές επιδόσεις και χρονισμούς, με built-in heat spreaders και multi-zone RGB φωτισμό που συμπληρώνει το στυλ κάθε gamer.", "Corsair",  port16, new Port(), 29);
         getComponentDAO().save(com16);
         total_cost2 = total_cost2.plus(com16.getPrice());
 
-        Component com17 = new Component(4311, Money.euros(BigDecimal.valueOf(189.90)), "Asus VGA GeForce GTX 1630 Phoenix EVO 4 GB", "Προσφέρει υψηλές επιδόσεις στα παιχνίδια χάρη στην αρχιτεκτονική NVIDIA Turing™. Διαθέτει 512 πυρήνες CUDA® και 4GB μνήμης GDDR6.", "Asus", new Port(), new Port(), 45);
+
+        Pair pair17_2 = new Pair("HDMI",1);
+        Pair pair17_3 = new Pair("DVI-D",1);
+        Pair pair17_4 = new Pair("DisplayPort",1);
+
+        Port port17 = new Port();
+
+        port17.add(pair17_3);
+        port17.add(pair17_2);
+        port17.add(pair17_4);
+        Component com17 = new Component(4311, Money.euros(BigDecimal.valueOf(189.90)), "Asus VGA GeForce GTX 1630 Phoenix EVO 4 GB", "Προσφέρει υψηλές επιδόσεις στα παιχνίδια χάρη στην αρχιτεκτονική NVIDIA Turing™. Διαθέτει 512 πυρήνες CUDA® και 4GB μνήμης GDDR6.", "Asus", port17, port7_1, 45);
         getComponentDAO().save(com17);
         total_cost2 = total_cost2.plus(com17.getPrice());
 
-        Component com18 = new Component(3748, Money.euros(BigDecimal.valueOf(94.90)), "Samsung SSD 980 NVMe M.2 1TB", "Το Samsung NVMe SSD χρησιμοποιεί τεχνολογία μνήμης V-NAND και υποστηρίζει ταχύτητες ανάγνωσης και εγγραφής (sequential) έως 3.500MB/s και 3.000MB/s αντίστοιχα. Έχει χωρητικότητα 1TB", "Samsung", new Port(), new Port(), 66);
+        Pair pair18 = new Pair("M.2",1);
+        Port port18 = new Port();
+
+        port18.add(pair18);
+        Component com18 = new Component(3748, Money.euros(BigDecimal.valueOf(94.90)), "Samsung SSD 980 NVMe M.2 1TB", "Το Samsung NVMe SSD χρησιμοποιεί τεχνολογία μνήμης V-NAND και υποστηρίζει ταχύτητες ανάγνωσης και εγγραφής (sequential) έως 3.500MB/s και 3.000MB/s αντίστοιχα. Έχει χωρητικότητα 1TB", "Samsung", new Port() , port18, 66);
         getComponentDAO().save(com18);
         total_cost2 = total_cost2.plus(com18.getPrice());
 
@@ -136,6 +235,7 @@ public abstract class Initializer {
         components1.add(com8);
         Synthesis s1 = new Synthesis(9787, total_cost1, "Synthesis1", components1);
         getSynthesisDAO().save(s1);
+
 
         ArrayList<Component> components2 = new ArrayList<Component>();
         components2.add(com11);
