9a38a656707f6660037f60be9349c06e0bc7786f
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-30 19:57:04 +0200

    added tests for start screen

---
+++ app/src/test/java/com/example/tecktrove/view/StartScreen/StartScreenPresenterTest.java
@@ -0,0 +1,53 @@
+package com.example.tecktrove.view.StartScreen;
+import com.example.tecktrove.view.StartScreen.StartScreenPresenter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StartScreenPresenterTest {
+    private StartScreenViewStub view;
+    private StartScreenPresenter presenter;
+
+    @Before
+    public void setUp() {
+        view = new StartScreenViewStub();
+        presenter = new StartScreenPresenter(view);
+    }
+
+    /**
+     * We test to see if the presenter calls the right method
+     **/
+    @Test
+    public void testLogIn() {
+        presenter.onLogIn();
+        Assert.assertEquals(1, view.getTimesVisitedLogIn());
+    }
+
+    @Test
+    public void testSignUp(){
+        presenter.onSignUp();
+        Assert.assertEquals(1,view.getTimesVisitedSignUp());
+    }
+}
+class StartScreenViewStub implements StartScreenView {
+    int timesVisitedLogIn = 0;
+    int timesVisitedSignUp = 0;
+
+    public int getTimesVisitedLogIn(){
+        return timesVisitedLogIn;
+    }
+    public int getTimesVisitedSignUp(){
+        return timesVisitedSignUp;
+    }
+
+    @Override
+    public void logInForm() {
+        timesVisitedLogIn++;
+    }
+
+    @Override
+    public void signUpForm() {
+        timesVisitedSignUp++;
+    }
+}
