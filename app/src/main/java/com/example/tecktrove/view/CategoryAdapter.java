f7b1266de60cf2b545dab0cfd71b4689c4cfb6eb
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-30 18:49:29 +0200

    fixed both adapters

--- app/src/main/java/com/example/tecktrove/view/CategoryAdapter.java
+++ app/src/main/java/com/example/tecktrove/view/CategoryAdapter.java
@@ -34,32 +34,23 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @Override
     public CategoryViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+
         return new CategoryViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder( CategoryViewHolder holder, int position) {
 
-        int firstCategoryPosition = position * 2;
-        int secondCategoryPosition = firstCategoryPosition + 1;
+        int CategoryPosition = position;
+        String Category = categoryList.get(CategoryPosition);
+        holder.bindCategory(Category);
 
-        // Bind data to the ViewHolder for the first product in the row
-        if (firstCategoryPosition < categoryList.size()) {
-            String firstCategory = categoryList.get(firstCategoryPosition);
-            holder.bindCategory(firstCategory, firstCategoryPosition, getItemCount());
-        }
-
-        // Bind data to the ViewHolder for the second product in the row
-        if (secondCategoryPosition < categoryList.size()) {
-            String secondProduct = categoryList.get(secondCategoryPosition);
-            holder.bindCategory(secondProduct, secondCategoryPosition, getItemCount());
-        }
     }
 
     @Override
     public int getItemCount() {
         Log.d("CategoryAdapter", "count");
-        return  (int) Math.ceil(categoryList.size()/2.0);
+        return categoryList.size();
     }
 
     public class CategoryViewHolder extends RecyclerView.ViewHolder {
@@ -78,7 +69,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
                     if (adapterPosition != RecyclerView.NO_POSITION) {
                         // Calculate the correct clicked category index based on the position
 
-                        int clickedCategoryIndex = adapterPosition * 2;
+                        int clickedCategoryIndex = adapterPosition;
 
                         // Check if the clicked position is within the bounds of the category list
                         if (clickedCategoryIndex < categoryList.size()) {
@@ -91,32 +82,19 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
             });
         }
 
-        void bindCategory(String category, int position, int categories) {
-            View gloytsi = itemView.findViewById(R.id.gloytsi);
-            if (position == ((int)categories*2.0) - 1 && categories % 2 != 0) {
-                // For the last row with odd number of categories, display only the odd category
-                gloytsi.setVisibility(View.GONE);
-            } else {
-                Log.d("CategoryAdapter", "gloy");
-                gloytsi.setVisibility(View.VISIBLE);
-
-                if (position % 2 == 1) {
-                    // Even position, use views for the second product
-                    categoryName = itemView.findViewById(R.id.productName2);
-                    categoryImage = itemView.findViewById(R.id.productImage2);
-                } else {
+        void bindCategory(String category) {
                     // Odd position, use views for the first product
-                    categoryName = itemView.findViewById(R.id.productName1);
-                    categoryImage = itemView.findViewById(R.id.productImage1);
-                }
-                categoryName.setText(category);
-                int resId = itemView.getResources().getIdentifier(category.toLowerCase(), "drawable", itemView.getContext().getPackageName());
+            categoryName = itemView.findViewById(R.id.productName1);
+            categoryImage = itemView.findViewById(R.id.productImage1);
 
-                // Set the image based on the resource identifier
-                if (resId != 0) {
-                    categoryImage.setImageResource(resId);
-                }
+            categoryName.setText(category);
+            int resId = itemView.getResources().getIdentifier(category.toLowerCase(), "drawable", itemView.getContext().getPackageName());
+
+            // Set the image based on the resource identifier
+            if (resId != 0) {
+                categoryImage.setImageResource(resId);
             }
+
 
 
 
--- app/src/main/java/com/example/tecktrove/view/ProductAdapter.java
+++ app/src/main/java/com/example/tecktrove/view/ProductAdapter.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 // ProductAdapter.java
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
 
-    private ArrayList<ProductType> productList; // Assume you have a Product class
+    private ArrayList<ProductType> productList;
 
     public ProductAdapter(ArrayList<ProductType> productList) {
         this.productList = productList;
@@ -31,25 +31,16 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
     @Override
     public void onBindViewHolder(ProductViewHolder holder, int position) {
-        int firstProductPosition = position * 2;
-        int secondProductPosition = firstProductPosition + 1;
+        int ProductPosition = position;
 
         // Bind data to the ViewHolder for the first product in the row
-        if (firstProductPosition < productList.size()) {
-            ProductType firstProduct = productList.get(firstProductPosition);
-            holder.bindProduct(firstProduct, firstProductPosition);
-        }
-
-        // Bind data to the ViewHolder for the second product in the row
-        if (secondProductPosition < productList.size()) {
-            ProductType secondProduct = productList.get(secondProductPosition);
-            holder.bindProduct(secondProduct, secondProductPosition);
-        }
+        ProductType Product = productList.get(ProductPosition);
+        holder.bindProduct(Product);
     }
 
     @Override
     public int getItemCount() {
-        return (int) Math.ceil(productList.size() / 2.0);
+        return productList.size();
     }
 
     static class ProductViewHolder extends RecyclerView.ViewHolder {
@@ -64,20 +55,13 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         }
 
         // Helper method to bind product data to the views
-        void bindProduct(ProductType product, int position) {
-            if (position % 2 == 0) {
-                // Even position, use views for the second product
-                productNameTextView = itemView.findViewById(R.id.productName2);
-                productImageView = itemView.findViewById(R.id.productImage2);
-            } else {
-                // Odd position, use views for the first product
-                productNameTextView = itemView.findViewById(R.id.productName1);
-                productImageView = itemView.findViewById(R.id.productImage1);
-            }
+        void bindProduct(ProductType product) {
+            productNameTextView = itemView.findViewById(R.id.productName1);
+            productImageView = itemView.findViewById(R.id.productImage1);
+
             productNameTextView.setText(product.getName());
             String s = product.getImg();
             //productImageView.setImageResource(R.drawable.s);
-            // You can add more bindings here based on your product data
         }
     }
 }
