74ff908d88107038d6de1d16f2a442071caedaa3
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-04 00:15:08 +0200

    Update SharedViewModel.java

--- app/src/main/java/com/example/tecktrove/view/SharedViewModel.java
+++ app/src/main/java/com/example/tecktrove/view/SharedViewModel.java
@@ -3,9 +3,12 @@ package com.example.tecktrove.view;
 import androidx.lifecycle.ViewModel;
 
 import com.example.tecktrove.domain.Customer;
+import com.example.tecktrove.domain.Employer;
 
 public class SharedViewModel extends ViewModel {
     private static Customer customer;
+    private static boolean isEmployer;
+    private static Employer employer;
 
     public Customer getCustomer() {
         return customer;
@@ -14,4 +17,13 @@ public class SharedViewModel extends ViewModel {
     public void setSharedCustomer(Customer customer) {
         this.customer = customer;
     }
+
+    public boolean isEmployer(){
+        return isEmployer;
+    }
+
+    public void setSharedEmployer(Employer employer){ this.employer = employer;}
+
+    public Employer getEmployer(){return this.employer;}
+    public void setIsEmployer(boolean isEmployer){this.isEmployer = isEmployer;}
 }
