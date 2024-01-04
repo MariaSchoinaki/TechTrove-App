313dcae12debc63e9149ff9ce2ed386defec7d24
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-04 16:35:37 +0200

    fixed search products

--- app/src/main/java/com/example/tecktrove/view/Employer/HomeScreen/HomeScreenPresenter.java
+++ app/src/main/java/com/example/tecktrove/view/Employer/HomeScreen/HomeScreenPresenter.java
@@ -2,40 +2,58 @@ package com.example.tecktrove.view.Employer.HomeScreen;
 
 import android.util.Log;
 
+import com.example.tecktrove.dao.ComponentDAO;
 import com.example.tecktrove.dao.CustomerDAO;
 import com.example.tecktrove.dao.EmployerDAO;
+import com.example.tecktrove.dao.SynthesisDAO;
+import com.example.tecktrove.domain.ProductType;
+
+import java.util.ArrayList;
 
 public class HomeScreenPresenter {
     private HomeScreenView view;
     CustomerDAO customers;
     EmployerDAO employers;
+    ComponentDAO componentDAO;
+    SynthesisDAO synthesisDAO;
+    ArrayList<ProductType> searchResults;
 
-    public HomeScreenPresenter(HomeScreenView view, CustomerDAO customers, EmployerDAO employers){
+    public HomeScreenPresenter(HomeScreenView view, CustomerDAO customers, EmployerDAO employers, ComponentDAO componentDAO, SynthesisDAO synthesisDAO){
         this.view = view;
         this.customers = customers;
         this.employers = employers;
+        this.componentDAO = componentDAO;
+        this.synthesisDAO = synthesisDAO;
     }
 
-    void onAll(){
-        view.All();
+    void onDisplayProducts(String filter){
+        searchProducts(filter);
+        view.displayProducts(searchResults);
     }
-    void onRam(){
-        view.Ram();
-    }
-    void onCpu(){
-        view.Cpu();
-    }
-    void onBox(){
-        view.Box();
-    }
-    void onMotherboard(){view.Motherboard();}
-    void onVga(){view.Vga();}
-    void onDisk(){view.Disk();}
-    void onCooler(){view.Cooler();}
-    void ontrofodotiko(){view.trofodotiko();}
-
 
     void onHome(){view.goToHome();}
 
     void onMyAccount(){view.goToMyAccount();}
+
+    private void searchProducts(String query) {
+        searchResults = new ArrayList<>();
+        ArrayList<ProductType> allProducts = new ArrayList<>(componentDAO.findAll());
+        allProducts.addAll(new ArrayList<>(synthesisDAO.findAll()));
+
+        if(query.equals("all")){
+            searchResults = allProducts;
+        }else {
+            String[] queries = query.split(" ");
+
+
+            for (ProductType prod : allProducts) {
+                for (String q : queries) {
+                    if (prod.getName().toLowerCase().contains(q.toLowerCase()) ||
+                            (String.valueOf(prod.getModelNo())).equals(q.toLowerCase())) {
+                        searchResults.add(prod);
+                    }
+                }
+            }
+        }
+    }
 }
