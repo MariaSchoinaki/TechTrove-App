5524fde7468add583d2856e26ea19db5703009fb
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-05 21:17:08 +0200

    added javadoc comments in util

--- app/src/main/java/com/example/tecktrove/util/Pair.java
+++ app/src/main/java/com/example/tecktrove/util/Pair.java
@@ -11,22 +11,46 @@ public class Pair<T, K> {
         this.second = i;
     }
 
+    /**
+     * Sets the first attribute of the pair
+     *
+     * @param first the first attribute
+     */
     public void setFirst(T first) {
         this.first = first;
     }
 
+    /**
+     * Sets the second attribute or the pair
+     * @param second    the second attribute
+     */
     public void setSecond(K second) {
         this.second = second;
     }
 
+    /**
+     * Gets the second attribute of the pair
+     * @return  a {@link K} object
+     */
     public K getSecond() {
         return second;
     }
 
+    /**
+     * Gets the firts attribute of the pair
+     * @return  a {@link T} object
+     */
     public T getFirst() {
         return first;
     }
 
+    /**
+     * Returns {@code true} if an object is equals
+     * with the object pair
+     *
+     * @param other the other object
+     * @return  true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object other) {
         if (other == null){
--- app/src/main/java/com/example/tecktrove/util/Port.java
+++ app/src/main/java/com/example/tecktrove/util/Port.java
@@ -13,21 +13,47 @@ import java.util.Objects;
 public class Port {
 
     private ArrayList<Pair<String,Integer>> ports;
+
+    /**
+     * Default constructor
+     */
     public Port(){
         this.ports = new ArrayList<Pair<String,Integer>>();
     }
 
+    /**
+     * Constructor with parameters: {ports}
+     * @param ports an ArrayList with {@link Pair} objects
+     *              representing with first the name of the port
+     *              and second the number of the ports
+     */
     public Port(ArrayList<Pair<String,Integer>> ports){
         this.ports = ports;
     }
+
+    /**
+     * Adds a pair of port in the ArrayList of Ports
+     *
+     * @param port a pair object <String nameofport, Integer numberofport>
+     */
     public void add(Pair port){
         ports.add(port);
     }
 
+    /**
+     * Removes a pair of port from the ArrayList
+     * @param p a pair object <String nameofport, Integer numberofport>
+     */
     public void remove(Pair p){
         ports.remove(p);
     }
 
+    /**
+     * Gets the number of ports a specific port has
+     *
+     * @param key   the name of the port
+     * @return  the number of ports or -1
+     */
     public int getPortNumber(String key) {
         Pair<String,Integer> p1 = findPortPair(key);
         for (Pair<String,Integer> p : ports) {
@@ -37,6 +63,14 @@ public class Port {
         }
         return -1;
     }
+
+    /**
+     * Returns {@code true} if another object is equal with this
+     * port object
+     *
+     * @param other the other object
+     * @return  {@code true} if the objects are equal, {@code false} otherwise
+     */
     @Override
     public boolean equals(Object other) {
         if (other == null){
@@ -62,10 +96,21 @@ public class Port {
         }
     }
 
+    /**
+     * Returns the ports
+     *
+     * @return  an ArrayList of {@link Pair} Objects
+     */
     public ArrayList<Pair<String,Integer>> getPorts(){
         return this.ports;
     }
 
+    /**
+     * Finds a Pair of port by the ports name
+     *
+     * @param key   the ports name
+     * @return      a {@link Pair} object or null
+     */
     public Pair<String,Integer> findPortPair(String key){
         for (Pair<String,Integer> p : ports) {
             if (p.getFirst() == key) {
