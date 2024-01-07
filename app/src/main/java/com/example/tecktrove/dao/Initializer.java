34bab77378c8e069537bf2d54ab6c3023af06c48
ChristosStamoulos <stamcrist2@outlook.com>
2024-01-07 18:42:45 +0200

    edited ports

--- app/src/main/java/com/example/tecktrove/dao/Initializer.java
+++ app/src/main/java/com/example/tecktrove/dao/Initializer.java
@@ -120,23 +120,24 @@ public abstract class Initializer {
         Component com2 = new Component(2936, Money.euros(BigDecimal.valueOf(36.90)), "Turbo-X PSU Value III Series 550 W", "Προσιτό αλλά αξιόπιστο τροφοδοτικό, με ισχύ 550W, προηγμένες δικλείδες ασφαλείας και αθόρυβο ανεμιστήρα 120mm.", "Turbo-X",  port2, new Port(), 75);
         getComponentDAO().save(com2);
 
-        Pair<String, Integer> pair3_1 = new Pair<String, Integer>("AM4",1);
+        Pair<String, Integer> pair3_1 = new Pair<String, Integer>("socket AM4",1);
         Port port3 = new Port();
         port3.add(pair3_1);
 
-        Component com3 = new Component(3260, Money.euros(BigDecimal.valueOf(119.90)), "AMD CPU Ryzen 3 3200G", "Με τέσσερις πυρήνες Zen σε Socket AM4, μέγιστη συχνότητα λειτουργίας 4GHz, μνήμη cache 6MB και Radeon Vega 8 iGPU για αξεπέραστες επιδόσεις γραφικών.", "AMD",port3,new Port(), 60);
+        Component com3 = new Component(3260, Money.euros(BigDecimal.valueOf(119.90)), "AMD CPU Ryzen 3 3200G", "Με τέσσερις πυρήνες Zen σε Socket AM4, μέγιστη συχνότητα λειτουργίας 4GHz, μνήμη cache 6MB και Radeon Vega 8 iGPU για αξεπέραστες επιδόσεις γραφικών.", "AMD",new Port(),port3, 60);
         getComponentDAO().save(com3);
 
-        Pair<String, Integer> pair4_1 = new Pair<String, Integer>("AM4,",1);
+        Pair<String, Integer> pair4_1 = new Pair<String, Integer>("socket AM4,",1);
         Port port4 = new Port();
         port4.add(pair4_1);
-        Component com4 = new Component(3888, Money.euros(BigDecimal.valueOf(32.90)), "Alpenföhn CPU Cooler Ben Nevis", "Ψύκτρα με ανεμιστήρα 130mm συμβατή με τα sockets 2066, 2011, 2011-v3, 1366, 115X, 1200, AM4, 775, AM4, AM3(+), AM3, AM2(+), AM2 και FM1.", "Alpenföhn",  port4, new Port(), 20);
+        Component com4 = new Component(3888, Money.euros(BigDecimal.valueOf(32.90)), "Alpenföhn CPU Cooler Ben Nevis", "Ψύκτρα με ανεμιστήρα 130mm συμβατή με τα sockets 2066, 2011, 2011-v3, 1366, 115X, 1200, AM4, 775, AM4, AM3(+), AM3, AM2(+), AM2 και FM1.", "Alpenföhn",  new Port(), port4, 20);
         getComponentDAO().save(com4);
 
-        Pair<String, Integer> pair5_1 = new Pair<String, Integer>("AM4",1);
-        Pair<String, Integer> pair5_2 = new Pair<String, Integer>("DDR4",2);
+        Pair<String, Integer> pair5_1 = new Pair<String, Integer>("socket AM4",1);
+        Pair<String, Integer> pair5_2 = new Pair<String, Integer>("socket DDR4",2);
         Pair<String, Integer> pair5_3 = new Pair<String, Integer>("PCI Express x16 3.0",1);
         Pair<String, Integer> pair5_4 = new Pair<String, Integer>("SATA III",4);
+
 
         Port port5 = new Port();
 
@@ -144,10 +145,11 @@ public abstract class Initializer {
         port5.add(pair5_2);
         port5.add(pair5_3);
         port5.add(pair5_4);
+
         Component com5 = new Component(4188, Money.euros(BigDecimal.valueOf(129.90)), "Gigabyte Motherboard A520I AC", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει DDR4 RAM ως 64GB, NVMe PCIe 3.0 M.2 και Intel® Dual Band 802.11ac Wi-Fi.", "AMD",port5, new Port(), 57);
         getComponentDAO().save(com5);
 
-        Pair<String, Integer> pair6 = new Pair<String, Integer>("DDR4",1);
+        Pair<String, Integer> pair6 = new Pair<String, Integer>("socket DDR4",1);
         Port port6 = new Port();
 
         port6.add(pair6);
@@ -171,7 +173,7 @@ public abstract class Initializer {
         Port port8 = new Port();
 
         port8.add(pair8);
-        Component com8 = new Component(2489, Money.euros(BigDecimal.valueOf(26.90)), "SanDisk SSD Plus 240GB", "Η τεχνολογία SSD 2,5” στο φορητό ή το σταθερό σου υπολογιστή, με χωρητικότητα 240GB, γρήγορες ταχύτητες λειτουργίας και σύνδεση SATA III.", "SanDisk",null,port8, 35);
+        Component com8 = new Component(2489, Money.euros(BigDecimal.valueOf(26.90)), "SanDisk SSD Plus 240GB", "Η τεχνολογία SSD 2,5” στο φορητό ή το σταθερό σου υπολογιστή, με χωρητικότητα 240GB, γρήγορες ταχύτητες λειτουργίας και σύνδεση SATA III.", "SanDisk",new Port(),port8, 35);
         getComponentDAO().save(com8);
 
         Component com11 = new Component(3604, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon Midi ATX Tower VS4-V Midi Tower", "Μινιμαλιστική σχεδίαση, ευρύχωρο εσωτερικό και άφθονες θέσεις για SSDs/HDDs, προεγκατεστημένος ανεμιστήρας και φίλτρο σκόνης, πρακτικό πάνελ και συμβατότητα με συστήματα υδρόψυξης.", "Sharkoon", new Port(), new Port(), 10);
@@ -187,13 +189,13 @@ public abstract class Initializer {
         port12.add(pair12_2);
         port12.add(pair12_3);
         port12.add(pair12_4);
-        Component com12 = new Component(3740, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon PSU Series 500 W 80+ Bronze", "Τροφοδοτικό υπολογιστή Sharkoon 500W με πιστοποίηση 80+ Bronze, active PFC, αθόρυβη λειτουργεία με ανεμιστήρα 120mm και διακριτική μαύρη εμφάνιση.", "Sharkoon",  port12,port2_1, 17);
+        Component com12 = new Component(3740, Money.euros(BigDecimal.valueOf(69.90)), "Sharkoon PSU Series 500 W 80+ Bronze", "Τροφοδοτικό υπολογιστή Sharkoon 500W με πιστοποίηση 80+ Bronze, active PFC, αθόρυβη λειτουργεία με ανεμιστήρα 120mm και διακριτική μαύρη εμφάνιση.", "Sharkoon",  port12 ,new Port(), 17);
         getComponentDAO().save(com12);
 
         Pair<String, Integer> pair13_1 = new Pair<String, Integer>("socket AM4+",1);
         Port port13 = new Port();
         port13.add(pair13_1);
-        Component com13 = new Component(3740, Money.euros(BigDecimal.valueOf(84.90)), "AMD CPU Ryzen 5 4500", "Με πυρήνες αρχιτεκτονικής Zen 2 και υψηλότερους χρονισμούς, περισσότερο bandwidth, υποστήριξη PCIe 3.0 και αποκλειστικές τεχνολογίες AMD, επαναπροσδιορίζουν την απόδοση των Gaming PCs.", "AMD",port13,port13,33);
+        Component com13 = new Component(3740, Money.euros(BigDecimal.valueOf(84.90)), "AMD CPU Ryzen 5 4500", "Με πυρήνες αρχιτεκτονικής Zen 2 και υψηλότερους χρονισμούς, περισσότερο bandwidth, υποστήριξη PCIe 3.0 και αποκλειστικές τεχνολογίες AMD, επαναπροσδιορίζουν την απόδοση των Gaming PCs.", "AMD",new Port(),port13,33);
         getComponentDAO().save(com13);
 
         Pair<String, Integer> pair14_1 = new Pair<String, Integer>("4-Pin PWM",1);
@@ -213,25 +215,30 @@ public abstract class Initializer {
         port15.add(pair15_2);
         port15.add(pair15_3);
         port15.add(pair15_4);
-        Component com15 = new Component(3639, Money.euros(BigDecimal.valueOf(69.90)), "MSI Motherboard A520M-A PRO", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει μνήμη DDR3 ως 64GB και έχει υποδοχές Μ.2 και PCI Express Gen3 x16.", "AMD", port15, new Port(), 85);
+        Component com15 = new Component(3639, Money.euros(BigDecimal.valueOf(69.90)), "MSI Motherboard A520M-A PRO", "Βασίζεται στο AMD® A520 Chipset και δέχεται επεξεργαστές AMD Ryzen™ 5000 και 3000 Series καθώς και 4000 G-Series. Υποστηρίζει μνήμη DDR5 ως 64GB και έχει υποδοχές Μ.2 και PCI Express Gen3 x16.", "AMD", port15, new Port(), 85);
         getComponentDAO().save(com15);
 
         Pair<String, Integer> pair16 = new Pair<String, Integer>("DDR5",1);
         Port port16 = new Port();
         port16.add(pair16);
-        Component com16 = new Component(3290, Money.euros(BigDecimal.valueOf(69.90)), "Corsair Desktop RAM Vengeance PRO RGB 16GB Kit 3000MHz DDR5", "16GB μνήμης RAM DDR5 για υψηλές επιδόσεις και χρονισμούς, με built-in heat spreaders και multi-zone RGB φωτισμό που συμπληρώνει το στυλ κάθε gamer.", "Corsair",  port16, new Port(), 29);
+        Component com16 = new Component(3290, Money.euros(BigDecimal.valueOf(69.90)), "Corsair Desktop RAM Vengeance PRO RGB 16GB Kit 3000MHz DDR5", "16GB μνήμης RAM DDR5 για υψηλές επιδόσεις και χρονισμούς, με built-in heat spreaders και multi-zone RGB φωτισμό που συμπληρώνει το στυλ κάθε gamer.", "Corsair", new Port(), port16, 29);
         getComponentDAO().save(com16);
 
 
         Pair<String, Integer> pair17_2 = new Pair<String, Integer>("HDMI",1);
         Pair<String, Integer> pair17_3 = new Pair<String, Integer>("DVI-D",1);
         Pair<String, Integer> pair17_4 = new Pair<String, Integer>("DisplayPort",1);
+        Pair<String, Integer> pair17_5 = new Pair<String, Integer>("PCI Express Connector",1);
+        Pair<String, Integer> pair17_6 = new Pair<String, Integer>("PCI Express x16 3.0",1);
 
         Port port17 = new Port();
+        Port port17_1 = new Port();
 
         port17.add(pair17_3);
         port17.add(pair17_2);
         port17.add(pair17_4);
+        port17_1.add(pair17_5);
+        port17_1.add(pair17_6);
         Component com17 = new Component(4311, Money.euros(BigDecimal.valueOf(189.90)), "Asus VGA GeForce GTX 1630 Phoenix EVO 4 GB", "Προσφέρει υψηλές επιδόσεις στα παιχνίδια χάρη στην αρχιτεκτονική NVIDIA Turing™. Διαθέτει 512 πυρήνες CUDA® και 4GB μνήμης GDDR6.", "Asus", port17, port7_1, 45);
         getComponentDAO().save(com17);
 
