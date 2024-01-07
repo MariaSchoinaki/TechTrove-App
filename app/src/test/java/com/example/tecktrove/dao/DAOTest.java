fa5f4504a55d187c5246ab21df4589c9ff63520c
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-07 23:12:10 +0200

    Update DAOTest.java

--- app/src/test/java/com/example/tecktrove/dao/DAOTest.java
+++ app/src/test/java/com/example/tecktrove/dao/DAOTest.java
@@ -50,6 +50,9 @@ public class DAOTest {
     private static final int INITIAL_REQUIRED_COUNT = 2;
     private static final int INITIAL_SYNTHESIS_COUNT = 2;
 
+    /**
+     * Sets up the daos and initializes the data
+     */
     @Before
     public void setUp() {
         Initializer dataHelper = new MemoryInitializer();
@@ -65,12 +68,18 @@ public class DAOTest {
         orderDAO = new OrderDAOMemory();
     }
 
+    /**
+     * Tests method find all customers in the dao
+     */
     @Test
     public void CustomerCheckFindAll(){
         Assert.assertEquals(3, customerDAO.findAll().size());
         Assert.assertTrue(customerDAO.findAll().contains(new Customer(5673, "george", "ok123456", "George", "Johnson", new Email("klap@yahoo.com"), new Telephone("6898909678"), new ArrayList<Synthesis>(), new ArrayList<OrderLine>())));
     }
 
+    /**
+     * Tests finding customer by their username and password
+     */
     @Test
     public void CustomerCheckFindByUsernameAndPassword(){
         Assert.assertEquals("Maria", customerDAO.findCustomerByUsernameAndPassword("maria5", "31m@ria5").getName());
@@ -78,35 +87,62 @@ public class DAOTest {
         Assert.assertEquals(new Customer(2598, "chris", "chr!s598", "Christos", "Papaioanou", new Email("papaio54@gmail.com"), new Telephone("6985369825"), new ArrayList<Synthesis>(), new ArrayList<OrderLine>()), customerDAO.findCustomerByUsernameAndPassword("chris","chr!s598"));
     }
 
+    /**
+     * Tests finding customer by their id
+     */
     @Test
     public void CustomerCheckFindById(){
         Assert.assertEquals(new Customer(2598, "chris", "chr!s598", "Christos", "Papaioanou", new Email("papaio54@gmail.com"), new Telephone("6985369825"), new ArrayList<Synthesis>(), new ArrayList<OrderLine>()), customerDAO.findCustomerByID(2598));
         Assert.assertNull(customerDAO.findCustomerByID(4));
     }
 
+    /**
+     * Tests saving new customer in the dao
+     */
     @Test
     public void CustomerCheckSave(){
         customerDAO.save(new Customer(34, "leon", "leon2001", "leonidas","fragkias", new Email("leon@gmail.com"), new Telephone("6972342376"), new ArrayList<Synthesis>(),new ArrayList<OrderLine>()));
         Assert.assertEquals(4, customerDAO.findAll().size());
     }
 
+    /**
+     * Tests deleting customer from the dao
+     */
     @Test
     public void CustomerCheckDelete(){
         customerDAO.delete(new Customer(7859, "maria5", "31m@ria5", "Maria", "Papadaki", new Email("papadaki27@gmail.com"), new Telephone("6984596936"), new ArrayList<Synthesis>(), new ArrayList<OrderLine>()));
         Assert.assertEquals(2, customerDAO.findAll().size());
     }
 
+    /**
+     * Tests next available id for a new customer
+     */
     @Test
     public void CustomerCheckNextId(){
         Assert.assertEquals(2599,customerDAO.nextId());
     }
 
+    /**
+     * Tests find customer by username
+     */
+    @Test
+    public void CustomerCheckFindByUsername(){
+        Assert.assertNull(customerDAO.findCustomerByUsername("yolo"));
+        Assert.assertNotNull(customerDAO.findCustomerByUsername("george"));
+    }
+
+    /**
+     * Tests method find all employers in the dao
+     */
     @Test
     public void EmployerCheckFindAll(){
         Assert.assertEquals(3, employerDAO.findAll().size());
         Assert.assertTrue(employerDAO.findAll().contains(new Employer(1252, "eleni3", "elen!562", "Eleni", "Georgali", new Email("eleniii2@gmail.com"), new Telephone("6988745961"))));
     }
 
+    /**
+     * Tests finding employer by their username and password
+     */
     @Test
     public void EmployerCheckFindByUsernameAndPassword(){
         Assert.assertEquals("Konstantinos", employerDAO.findEmployerByUsernameAndPassword("kostas34", "13k023m32").getName());
@@ -114,29 +150,44 @@ public class DAOTest {
         Assert.assertEquals(new Employer(5698, "faihh1", "faihskoni1", "Foteini", "Soultatou", new Email("apple3r@yahoo.com"), new Telephone("6978451236")), employerDAO.findEmployerByUsernameAndPassword("faihh1","faihskoni1"));
     }
 
+    /**
+     * Tests finding employer by username
+     */
     @Test
     public void EmployerCheckFindByUsername(){
         Assert.assertNull(employerDAO.findEmployerByUsername("yolo"));
         Assert.assertNotNull(employerDAO.findEmployerByUsername("eleni3"));
     }
 
+    /**
+     * Tests next available employer id
+     */
     @Test
     public void EmployerCheckNextId(){
         Assert.assertEquals(8597,employerDAO.nextId());
     }
 
+    /**
+     * Tests finding employer by their id
+     */
     @Test
     public void EmployerCheckFindById(){
         Assert.assertEquals(new Employer(8596, "kostas34", "13k023m32", "Konstantinos", "Glitsas", new Email("glitsas13@gmail.com"), new Telephone("6945259875")), employerDAO.findEmployerByID(8596));
         Assert.assertNull(employerDAO.findEmployerByID(0));
     }
 
+    /**
+     * Tests saving new employer in the dao
+     */
     @Test
     public void EmployerCheckSave(){
         employerDAO.save(new Employer(34, "ippo", "ippo1999", "Ippokratis","Morfonios", new Email("ippomorf@gmail.com"), new Telephone("6972247306")));
         Assert.assertEquals(4, employerDAO.findAll().size());
     }
 
+    /**
+     * Tests deleting an employer from the dao
+     */
     @Test
     public void EmployerCheckDelete(){
         employerDAO.delete(new Employer(8596, "kostas34", "13k023m32", "Konstantinos", "Glitsas", new Email("glitsas13@gmail.com"), new Telephone("6945259875")));
@@ -145,23 +196,35 @@ public class DAOTest {
         Assert.assertEquals(2, employerDAO.findAll().size());
     }
 
+    /**
+     * Tests finding a model number that does not exist
+     */
     @Test
     public void findNonExistingModelNo(){
         Assert.assertNull(componentDAO.find(123456));
         Assert.assertNull(synthesisDAO.find(342156));
     }
 
+    /**
+     * Tests finding a name that does not exist
+     */
     @Test
     public void findNonExistingName(){
         Assert.assertNull(componentDAO.find("somalia"));
         Assert.assertNull(synthesisDAO.find("izola"));
     }
 
+    /**
+     * Tests finding a manufacturer that does not exist
+     */
     @Test
     public void findNonExistingManufacturer(){
         Assert.assertEquals(new ArrayList<Component>(),componentDAO.findByManufacturer("Lola"));
     }
 
+    /**
+     * Tests finding required ports that do not exist
+     */
     @Test
     public void findNonExistingRequiredPorts(){
         Pair p = new Pair("HDMI535",1);
@@ -170,6 +233,9 @@ public class DAOTest {
         Assert.assertEquals(new ArrayList<Component>(),componentDAO.findAllByRequiredPorts(port));
     }
 
+    /**
+     * Tests finding available ports that do not exist
+     */
     @Test
     public void findNonExistingAvailablePorts(){
         Pair p = new Pair("HDMI535",1);
@@ -178,12 +244,18 @@ public class DAOTest {
         Assert.assertEquals(new ArrayList<Component>(),componentDAO.findAllByAvailablePorts(port));
     }
 
+    /**
+     * Tests finding all the components in the dao
+     */
     @Test
     public void listAllComponents(){
         ArrayList<Component> allComponents = componentDAO.findAll();
         Assert.assertEquals(INITIAL_COMPONENTS_COUNT, allComponents.size());
     }
 
+    /**
+     * Tests saving a new component in the dao
+     */
     @Test
     public void testSaveComponents(){
         Component component = new Component(12,new Money(BigDecimal.valueOf(123), Currency.getInstance("EUR")),"display","very good","SAMSUNG ELECTRONICS CO&LTD",new Port(),new Port(),13);
@@ -194,16 +266,25 @@ public class DAOTest {
         Assert.assertTrue(componentDAO.findAll().contains(component));
     }
 
+    /**
+     * Tests finding synthesis of a rating that do not exist
+     */
     @Test
     public void findNonExistingRating(){
         Assert.assertEquals(new ArrayList<Synthesis>(),synthesisDAO.findAllByRating(12.2));
     }
 
+    /**
+     * Tests finding synthesis of a number of ratings that does not exist
+     */
     @Test
     public void findNonExistingNumberOfRatings(){
         Assert.assertEquals(new ArrayList<Synthesis>(),synthesisDAO.findAllByNumberOfRatings(5));
     }
 
+    /**
+     * Tests saving a new synthesis in the dao
+     */
     @Test
     public void testSaveSynthesis(){
         Synthesis s1 = new Synthesis(235456, Money.euros(1500), "Best Synthesis");
@@ -213,6 +294,10 @@ public class DAOTest {
         Assert.assertNotNull(synthesisDAO.find("Best Synthesis"));
         Assert.assertTrue(synthesisDAO.findAll().contains(s1));
     }
+
+    /**
+     * Tests finding a sythesis in the dao
+     */
     @Test
     public void findSynthesis(){
         Synthesis s3 = new Synthesis();
@@ -227,6 +312,10 @@ public class DAOTest {
 
         Assert.assertEquals(2, synthesisDAO.findAllPublished().size());
     }
+
+    /**
+     * Tests finding all components by their available ports
+     */
     @Test
     public void testAvailablePorts(){
 
@@ -250,6 +339,9 @@ public class DAOTest {
         Assert.assertEquals(INITIAL_AVAILABLE_COUNT,componentDAO.findAllByAvailablePorts(port2).size());
     }
 
+    /**
+     * Tests finding all the components by their required ports
+     */
     @Test
     public void testRequiredPorts(){
 
@@ -262,6 +354,9 @@ public class DAOTest {
 
     }
 
+    /**
+     * Tests deleting all components by their required ports
+     */
     @Test
     public void testDeleteByRequiredPorts(){
         Pair p = new Pair("PCI Express x16 2.0",1);
@@ -274,6 +369,9 @@ public class DAOTest {
 
     }
 
+    /**
+     * Tests deleting all components by their available ports
+     */
     @Test
     public void testDeleteByAvailablePorts(){
 
@@ -298,6 +396,9 @@ public class DAOTest {
         Assert.assertEquals(INITIAL_COMPONENTS_COUNT-1,componentDAO.findAll().size());
     }
 
+    /**
+     * Tests deleting all components by their manufacturer
+     */
     @Test
     public void testDeleteByManufacturer(){
         Assert.assertEquals(2,componentDAO.findByManufacturer("Sharkoon").size());
@@ -307,6 +408,9 @@ public class DAOTest {
 
     }
 
+    /**
+     * Tests deleting a component
+     */
     @Test
     public void ComponentDelete(){
         Component com1 = new Component();
@@ -320,6 +424,9 @@ public class DAOTest {
         Assert.assertEquals(INITIAL_COMPONENTS_COUNT-2,componentDAO.findAll().size());
     }
 
+    /**
+     * Tests deleting and saving a synthesis
+     */
     @Test
     public void SynthesisDeleteAndSave(){
         Synthesis s1 = new Synthesis();
