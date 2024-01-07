b8c7566c8995320dab4c20d3a93680fff8813310
MariaSchoinaki <shinaki54@gmail.com>
2024-01-08 01:38:23 +0200

    added java docs

--- app/src/main/java/com/example/tecktrove/view/Employer/Product/AddProduct/AddProductActivity.java
+++ app/src/main/java/com/example/tecktrove/view/Employer/Product/AddProduct/AddProductActivity.java
@@ -20,6 +20,10 @@ public class AddProductActivity extends AppCompatActivity implements AddProductV
     private AddProductPresenter presenter;
     private Initializer init;
 
+    /**
+     * Initializes the classes attributes
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +47,21 @@ public class AddProductActivity extends AppCompatActivity implements AddProductV
         });
     }
 
+    /**
+     * Helps return the navigation back to home screen
+     */
     @Override
     public void goToHomeScreen() {
         Intent intent = new Intent(this, HomeScreenActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Shows a pop up window with a customized message
+     *
+     * @param title the title of the window
+     * @param msg   the text of the window
+     */
     @Override
     public void showMessage(String title, String msg) {
         new AlertDialog.Builder(AddProductActivity.this)
@@ -58,41 +71,81 @@ public class AddProductActivity extends AppCompatActivity implements AddProductV
                 .setPositiveButton(R.string.ok, null).create().show();
     }
 
+    /**
+     * Gets the name
+     *
+     * @return name as a String
+     */
     @Override
     public String getName() {
         return ((EditText)findViewById(R.id.employer_product_change_name)).getText().toString().trim();
     }
 
+    /**
+     * Gets the price
+     *
+     * @return the price as a String
+     */
     @Override
     public String getPrice() {
         return ((EditText)findViewById(R.id.employer_product_change_price)).getText().toString().trim();
     }
 
+    /**
+     * Gets the manufacturer
+     *
+     * @return the manufacturer as a String
+     */
     @Override
     public String getManufacturer() {
         return ((EditText)findViewById(R.id.employer_product_change_manufacturer)).getText().toString().trim();
     }
 
+    /**
+     * Gets the description
+     *
+     * @return the description as a String
+     */
     @Override
     public String getDescription() {
         return ((EditText)findViewById(R.id.employer_product_change_description)).getText().toString().trim();
     }
 
+    /**
+     * Gets the model number
+     *
+     * @return the model number as a String
+     */
     @Override
     public String getModelNo() {
         return ((EditText)findViewById(R.id.employer_product_change_modelNo)).getText().toString().trim();
     }
 
+    /**
+     * Gets the available ports
+     *
+     * @return the available ports as a String
+     */
     @Override
     public String getAvailablePorts() {
         return ((EditText)findViewById(R.id.employer_product_change_available_ports)).getText().toString().trim();
     }
 
+    /**
+     * Gets the required ports
+     *
+     * @return the required ports as a String
+     */
     @Override
     public String getRequiredPorts() {
         return ((EditText)findViewById(R.id.employer_product_change_required_ports)).getText().toString().trim();
     }
 
+    /**
+     * Gets the quantity
+     *
+     * @return the quantity as an Integer
+     */
     @Override
     public int getQuantity() {
         return Integer.parseInt(((EditText) findViewById(R.id.employer_product_info_quantity)).getText().toString().trim());
